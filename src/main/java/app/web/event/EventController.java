6b95b84a6d2d00a8b0b14e73c6becb5c1ca9e947
Emre <emre139121@gmail.com>
2026-06-22 20:15:54 +0300

    refactor - EventController and some empty package removal

--- src/main/java/app/web/event/EventController.java
+++ src/main/java/app/web/event/EventController.java
@@ -1,4 +1,4 @@
-package app.web.trip;
+package app.web.event;
 
 import app.mapper.user.UserMapper;
 import app.model.dto.event.EventDto;
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.UUID;
